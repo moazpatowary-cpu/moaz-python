6ae27296fbf666bec6b3813716e724f5553d707d
moazpatowary-cpu <moazahmed420420@gmail.com>
2026-09-21 20:34:10 +0600

    Initial commit

---
+++ moaz.py
@@ -0,0 +1,9 @@
+number_pad = ((1,2,3),
+              (4,5,6),
+              (7,8,9),
+              ("*",0,"#"))
+for row in number_pad :
+    for columnn in row :
+        print(columnn, end=" ")
+    print()
+
